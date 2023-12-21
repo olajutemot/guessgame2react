3998a4ece5f058d45c96509a86b33bfcc5ea3f47
Olajutemo Timilehin <olajutemot@gmail.com>
2023-12-21 12:31:41 -0800

    update

--- src/Main.js
+++ src/Main.js
@@ -1,19 +1,49 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
+const messages = {
+  start: "Start guessing...",
+  input: "input a number",
+  high: "too High",
+  low: "too low",
+  correct: "you are correct",
+};
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(secretNumber);
 const Main = () => {
+  const [message, setMessage] = useState(messages.start);
+
+  const inputRef = useRef("");
+  let inputValue = inputRef.current.value;
+  // console.log(inputValue);
+
+  const handleCheck = () => {
+    if (!inputValue) {
+      setMessage(messages.input);
+    } else if (inputValue === secretNumber) {
+      setMessage(messages.correct);
+    } else if (inputValue > secretNumber) {
+      setMessage(messages.high);
+    } else if (inputValue < secretNumber) {
+      setMessage(messages.low);
+    }
+  };
+
   return (
     <main>
-      <section class="left">
-        <input type="number" class="guess" />
-        <button class="btn check">Check!</button>
+      <section className="left">
+        <input type="number" className="guess" ref={inputRef} />
+        <button className="btn check" onClick={handleCheck}>
+          Check!
+        </button>
       </section>
-      <section class="right">
-        <p class="message">Start guessing...</p>
-        <p class="label-score">
-          💯 Score: <span class="score">20</span>
+      <section className="right">
+        <p className="message"> {message} </p>
+        <p className="label-score">
+          💯 Score: <span className="score">20</span>
         </p>
-        <p class="label-highscore">
-          🥇 Highscore: <span class="highscore">0</span>
+        <p className="label-highscore">
+          🥇 Highscore: <span className="highscore">0</span>
         </p>
       </section>
     </main>
--- src/header.js
+++ src/header.js
@@ -4,9 +4,9 @@ const Header = () => {
   return (
     <header>
       <h1>Guess My Number!</h1>
-      <p class="between">(Between 1 and 20)</p>
-      <button class="btn again">Again!</button>
-      <div class="number">?</div>
+      <p className="between">(Between 1 and 20)</p>
+      <button className="btn again">Again!</button>
+      <div className="number">?</div>
     </header>
   );
 };
